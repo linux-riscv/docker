14740b698fb63f17693c6b51f75c5064e4afdfdf
Björn Töpel <bjorn@rivosinc.com>
2023-12-08 20:08:54 +0100

    Make it easier to bump toolchain version
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- setup-kernel-toolchain.sh
+++ setup-kernel-toolchain.sh
@@ -16,14 +16,23 @@ case ${arch} in
     ;;
 esac
 
+# Bump version here
+gcc_ver=13.2.0
+llvm_ver=17.0.6
+###
+
+gcc_base=gcc-${gcc_ver}-nolibc
+llvm_dir=llvm-${llvm_ver}-${arch}
+
 mkdir /opt || true
 cd /opt
-curl -O https://mirrors.edge.kernel.org/pub/tools/llvm/files/llvm-17.0.6-${arch}.tar.xz
-tar xf llvm-17.0.6-${arch}.tar.xz
 
-curl -O https://mirrors.edge.kernel.org/pub/tools/crosstool/files/bin/${tcarch}/13.2.0/${tcarch}-gcc-13.2.0-nolibc-riscv64-linux.tar.xz
-tar xf ${tcarch}-gcc-13.2.0-nolibc-riscv64-linux.tar.xz
-
+llvm=${llvm_dir}.tar.xz
+curl -O https://mirrors.edge.kernel.org/pub/tools/llvm/files/${llvm}
+tar xf ${llvm}
+gcc=${tcarch}-${gcc_base}-riscv64-linux.tar.xz
+curl -O https://mirrors.edge.kernel.org/pub/tools/crosstool/files/bin/${tcarch}/${gcc_ver}/${gcc}
+tar xf ${gcc}
 rm *.xz
 
 export PIPX_HOME=/root/.local/pipx/venvs
@@ -32,7 +41,7 @@ pipx install tuxmake
 pipx install dtschema
 
 echo 'export PATH=${PATH}:/root/.local/bin' >> /etc/profile
-echo "export PATH=/opt/gcc-13.2.0-nolibc/riscv64-linux/bin:/opt/gcc-13.2.0-nolibc/riscv32-linux/bin:/opt/llvm-17.0.2-${arch}/bin:\${PATH}" >> /etc/profile
+echo "export PATH=/opt/${gcc_base}/riscv64-linux/bin:/opt/${llvm_dir}/bin:\${PATH}" >> /etc/profile
 echo 'export CCACHE_DIR=/ccache' >> /etc/profile
 echo 'export CCACHE_MAXSIZE="50G"' >> /etc/profile
 
