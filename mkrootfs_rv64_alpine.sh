a14dfba8376116ed220e39d794a3f17789acd142
Björn Töpel <bjorn@rivosinc.com>
2024-09-11 05:37:04 +0000

    Bump Alpine
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- mkrootfs_rv64_alpine.sh
+++ mkrootfs_rv64_alpine.sh
@@ -17,7 +17,7 @@ trap 'rm -rf "$root"' EXIT
 arch=$(uname -m)
 
 cd $root
-curl -O http://dl-cdn.alpinelinux.org/alpine/edge/main/${arch}/apk-tools-static-2.14.4-r2.apk
+curl -O http://dl-cdn.alpinelinux.org/alpine/edge/main/${arch}/apk-tools-static-2.14.4-r3.apk
 tar xvf *.apk
 
 ./sbin/apk.static --arch riscv64 \
